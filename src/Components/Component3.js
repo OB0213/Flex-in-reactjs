004317cc6a408be5f77cb3fb14a3ed2966f862a3
Omkar-Bhosale-912 <omkarbhosale912@gmail.com>
2023-10-26 15:49:37 +0530

    Another Important Project

---
+++ src/Components/Component3.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.css'
+
+function Component3() {
+  return (
+    <div className='mymaindata'>
+      <h1 className='titledata'>Mumbai</h1>
+      <img src='https://media.istockphoto.com/id/1334792855/photo/chhatrapati-shivaji-terminus-in-mumbai-india.jpg?s=612x612&w=0&k=20&c=_zmL4EH_yMPC8-zleghoy_Z_JEelsiRJmNWoNUSZdj0=' className='myimage'></img>
+      <p className='textdata'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in faucibus felis, at accumsan ex. Proin vitae egestas enim, non ultricies nisi. Etiam interdum leo tellus, non posuere velit tempor eu. Vestibulum facilisis commodo porttitor. Phasellus vulputate tellus arcu, a ullamcorper metus porta eu. Maecenas feugiat finibus justo eu laoreet. Donec luctus sed sem at feugiat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Quisque massa augue, posuere eu libero ut, fringilla accumsan sem. Nam euismod mollis sem ac consequat. Mauris et nunc luctus, mattis nulla sed, facilisis sapien. Vestibulum tincidunt elementum nunc, a volutpat eros dignissim id. Fusce semper, dolor ac fringilla fringilla, eros dolor molestie dui, et auctor tellus massa a sem. Cras vel suscipit lacus. Nulla dignissim ante ipsum, nec cursus velit facilisis eu.
+
+Aenean sit amet faucibus arcu, eu cursus sem. Maecenas rhoncus nunc sit amet nibh interdum gravida. Nulla ultrices maximus mollis. Fusce rutrum condimentum dignissim. Donec vel arcu maximus, luctus est ut, feugiat augue. Donec ac diam tempus, aliquam ex a, luctus ante. Sed mi nisi, sodales eget semper sed, molestie sed magna. Integer sollicitudin auctor arcu vel lacinia. Aenean euismod ultricies augue in vestibulum. Fusce eu pellentesque metus. Cras vel massa condimentum, vulputate leo faucibus, accumsan est. Aenean varius facilisis elit eu elementum. Maecenas vel lacus vitae justo luctus egestas. Donec pretium magna ligula, lobortis mollis velit volutpat non. Sed euismod efficitur felis, non ultricies velit condimentum id.
+        </p>
+    </div>
+  )
+}
+
+export default Component3
